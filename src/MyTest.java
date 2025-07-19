51fffeac08d2c656a1494c199ed298a83e73917d
eman faisal emair <emanimair2@gmail.com>
2025-07-19 22:53:16 +0300

    Test003

---
+++ src/MyTest.java
@@ -0,0 +1,267 @@
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
+
+
+public class MyTest extends TestData {
+
+
+    
+	 @BeforeTest
+	 public void Mysetup() {
+	    	
+	  driver.get(URL);
+	  driver.manage().window().maximize(); 
+	        	
+	 }
+    @Test(priority = 1 , enabled = false)
+    public void T001_WebsiteLang_en() {
+    	
+    	
+    	String ActualLang_001 = driver.findElement(By.tagName("html")).getDomAttribute("lang"); 
+    	
+    	Assert.assertEquals(ActualLang_001, ExpectedLang_001);
+    	
+    }
+    
+    
+    
+    @Test(priority = 2 , enabled = true) 
+    public void T002_SearchFunctionality() {
+    	
+    	WebElement SearchLink = driver.findElement(By.className("StripNav")).findElements(By.tagName("li")).get(0);  
+    	SearchLink.click(); 
+    	
+    	
+    	WebElement SearchInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("txtGlobalSearch")));
+    	
+    	SearchInput.sendKeys(SearchKeyInput + Keys.ENTER);
+    	
+    	WebElement SearchResultElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("txtResult"))); 
+    	String SearchResultText = SearchResultElement.getText(); 
+    	
+    	boolean ActualResult_002 = SearchResultText.toLowerCase().contains(SearchKeyInput); 
+    	
+    	Assert.assertEquals(ActualResult_002, ExpectedResult_002);
+    }
+    
+    @Test(priority = 3 , enabled = false)
+    public void T003_ReqAppointment() throws InterruptedException {
+    	
+    	driver.findElement(By.linkText("Request an Appointment")).click(); 
+    	WebElement AppointmentForm =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("online-forms"))); 
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", AppointmentForm);
+        
+        WebElement FirstNameInput = AppointmentForm.findElement
+        (By.id
+        		("fxb_d1379fba-5889-4831-a334-07b9d78d7568_Fields_f9299103-b3d3-47e0-a64d-ef989e20b1f0__Value")); 
+        WebElement LastNameInput = AppointmentForm.findElement
+        		(By.id
+        				("fxb_d1379fba-5889-4831-a334-07b9d78d7568_Fields_a90b41b2-7e51-4198-93e7-a11def3c1fb1__Value")); 
+        
+        
+        FirstNameInput.sendKeys(FirstName); 
+        LastNameInput.sendKeys(LastName);
+        
+        WebElement DateOfBirth = AppointmentForm.findElement
+        		(By.id("fxb_d1379fba-5889-4831-a334-07b9d78d7568_Fields_841fa6ea-d1c2-44aa-a298-59259448e651__Value")); 
+        DateOfBirth.sendKeys(BirthMonth);
+        DateOfBirth.sendKeys(BirthDay);
+        DateOfBirth.sendKeys(BirthYear + Keys.ENTER);
+        
+        //gender input 
+  
+       String GenderValue = Gender.substring(0,1).toUpperCase() + Gender.substring(1);
+       
+       WebElement GenderButton = AppointmentForm.findElement
+    		    (By.xpath("//input[@value='" + GenderValue + "']")); 
+       
+       ((JavascriptExecutor) driver).executeScript("arguments[0].click();", GenderButton);
+       
+
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    WebElement countryDropdown = wait.until(ExpectedConditions.elementToBeClickable(
+        By.cssSelector("#fxb_d1379fba_5889_4831_a334_07b9d78d7568_Fields_c11a4935_eee4_48a2_8d54_6b7c1568ac61__Value_select_chosen")
+    ));
+
+    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", countryDropdown);
+    Thread.sleep(500); // wait for smooth scrolling
+
+    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", countryDropdown);
+
+    WebElement searchInput = wait.until(ExpectedConditions.visibilityOfElementLocated(
+        By.cssSelector(".chosen-search input")
+    ));
+    searchInput.sendKeys(Country_Sign);
+
+    WebElement countryResult = wait.until(ExpectedConditions.elementToBeClickable(
+        By.cssSelector(".chosen-results li.active-result")
+    ));
+    countryResult.click();
+
+    WebElement phoneInput = wait.until(ExpectedConditions.elementToBeClickable(
+        By.id("fxb_d1379fba-5889-4831-a334-07b9d78d7568_Fields_c11a4935-eee4-48a2-8d54-6b7c1568ac61__Value_input")
+    ));
+    phoneInput.sendKeys(PhoneNumber + Keys.ENTER ); 
+    
+    //email 
+    AppointmentForm.findElement
+    (By.id("fxb_d1379fba-5889-4831-a334-07b9d78d7568_Fields_2e1e8cd7-6984-4dd8-ae42-60a340153cde__Value")).
+    sendKeys(Email );
+    
+    String ID_value = "false"; 
+    if(Emirates_Id) {
+    	
+    	ID_value = "true"; 
+    }
+
+    List<WebElement> ID_Buttons = AppointmentForm.findElements(By.xpath("//input[@data-sc-field-name='rdbEmirates']"));
+    
+    for(WebElement button : ID_Buttons ) {
+    	
+    	String Value = button.getDomAttribute("value"); 
+    	   if (Value != null && Value.equals(ID_value)) {
+               // Scroll into view
+               ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", button);
+
+               // Optional: use JS click if normal click fails
+               try {
+                   button.click();
+               } catch (ElementNotInteractableException e) {
+                   System.out.println("Standard click failed, using JavaScript click...");
+                   ((JavascriptExecutor) driver).executeScript("arguments[0].click();", button);
+               }
+
+               System.out.println("Clicked radio with value: " + Value);
+               break;
+           }
+    }
+    		
+   
+    //country 
+    WebElement CountryDiv = AppointmentForm.findElement(By.id("fxb_d1379fba_5889_4831_a334_07b9d78d7568_Fields_41d924c5_1ada_4560_a73f_faf3fd8b07ea__Value_chosen")); 
+    
+    WebElement SelectCountryButton = CountryDiv.findElement(By.cssSelector(".chosen-single.chosen-default"));
+    ((JavascriptExecutor) driver ).executeScript("arguments[0].click()",SelectCountryButton );
+    
+    CountryDiv.findElement(By.className("chosen-search-input")).sendKeys(Country + Keys.ENTER); 
+     
+    
+    //first visit 
+    
+    String VisitValue = "false"; 
+    if (FirstVisit) {
+    	VisitValue = "true"; 
+    	
+    }
+    
+    List<WebElement> VisitTypeButtons = AppointmentForm.findElements(By.xpath("//input[@data-sc-field-name='rdbVisit']"));
+    for (WebElement button : VisitTypeButtons) {
+        String value = button.getDomAttribute("value");
+
+        if (value != null && value.equals(VisitValue)) {
+            // Scroll into view
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", button);
+
+            // Optional: use JS click if normal click fails
+            try {
+                button.click();
+            } catch (ElementNotInteractableException e) {
+                System.out.println("Standard click failed, using JavaScript click...");
+                ((JavascriptExecutor) driver).executeScript("arguments[0].click();", button);
+            }
+
+            System.out.println("Clicked radio with value: " + value);
+            break;
+        }
+    	
+    }
+    
+    //service 
+    
+    WebElement ServiceDiv = AppointmentForm.findElement
+    		(By.id("fxb_d1379fba_5889_4831_a334_07b9d78d7568_Fields_b10c772d_c6a4_40ab_b0ed_c45f57a82c54__Value_chosen")); 
+    
+    WebElement SelectServiceButton = ServiceDiv.findElement(By.cssSelector(".chosen-single.chosen-default"));
+    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", SelectServiceButton);
+    Thread.sleep(500);
+    ((JavascriptExecutor) driver ).executeScript("arguments[0].click()",SelectServiceButton );
+    
+    ServiceDiv.findElement(By.className("chosen-search-input")).sendKeys(Service + Keys.ENTER); 
+    
+    //why visiting 
+    WebElement TextArea = AppointmentForm.findElement(By.id("fxb_d1379fba-5889-4831-a334-07b9d78d7568_Fields_898bae0e-53f5-4c7d-b25d-9fed3586f84b__Value")); 
+    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", TextArea ); 
+    Thread.sleep(500); 
+    TextArea.sendKeys(WhyVisit + Keys.ENTER); 
+    
+    //Health Insurance - 
+    
+    String HaveHealthInsurance = "false"; 
+    if(HealthInsurance) {
+    	HaveHealthInsurance = "Health Insurance"; 
+    }else {
+    	
+    	HaveHealthInsurance = "Self Payment"; 
+
+    }
+    
+    
+    List<WebElement> HealthInsuranceButtons = AppointmentForm.findElements(By.xpath("//input[@data-sc-field-name='rdbInsurance']")); 
+    
+    for (WebElement button : HealthInsuranceButtons) {
+    	
+    	 String value = button.getDomAttribute("value");
+
+         if (value != null && value.equals(HaveHealthInsurance)) {
+             // Scroll into view
+             ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", button);
+
+             // Optional: use JS click if normal click fails
+             try {
+                 button.click();
+             } catch (ElementNotInteractableException e) {
+                 System.out.println("Standard click failed, using JavaScript click...");
+                 ((JavascriptExecutor) driver).executeScript("arguments[0].click();", button);
+             }
+
+             System.out.println("Clicked radio with value: " + value);
+             break;
+         }
+     	
+    } 
+    //Privacy Concerns 
+    
+    WebElement PrivacyButton = AppointmentForm.findElement(By.id("fxb_d1379fba-5889-4831-a334-07b9d78d7568_Fields_13ef172f-d53d-45db-b6c5-546c852f88b6__Value")); 
+    
+    ((JavascriptExecutor) driver).executeScript("arguments[0].click()", PrivacyButton); 
+    
+    
+    WebElement SubmitButton = AppointmentForm.findElement(By.xpath("//input[@value='Submit Information']")); 
+    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", SubmitButton);  
+    
+    WebElement ThankYouDiv = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("ThankyouInfo"))); 
+    Boolean ExpectedResult_003 = true; 
+    Boolean ActualResult_003 = ThankYouDiv.findElement(By.tagName("p")).getText().toLowerCase().contains("successfully"); 
+    
+    Assert.assertEquals(ActualResult_003, ExpectedResult_003); 
+    }// End of Test 
+    
+   
+
+    
+} 
---
+++ src/TestData.java
@@ -0,0 +1,45 @@
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+
+public class TestData {
+
+	
+	
+	
+	String URL  = "https://www.clevelandclinicabudhabi.ae"; 
+    WebDriver driver = new ChromeDriver();
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); 
+
+   
+    //Test001
+	String ExpectedLang_001 = "en"; 
+
+	//Test002
+	String SearchKeyInput = "cancer"; 
+	boolean ExpectedResult_002 = true;
+	
+	//Test003 
+	
+	String Email = "layanomar417@gmail.com"; 
+	String FirstName = "layan"; 
+	String LastName = "Omar"; 
+	String Country_Sign = "+962";
+	String PhoneNumber = "799646707" ; 
+	
+	String Gender = "female" ; 
+	Boolean  Emirates_Id = false; 
+	String Country = "Jordan"; 
+	Boolean FirstVisit = true; 
+	String BirthDay = "3"; 
+	String BirthMonth = "12" ; 
+	String BirthYear="001998" ; 
+	String Service = "Digestive Diseases";
+	Boolean HealthInsurance = false; 
+	String WhyVisit = "I have many gut problems";
+	//Test 004 
+	Boolean ExpectedResult_004 = true; 
+}
